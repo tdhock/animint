a9f1f6b403c004752678efb5049b5681c32cabd7
Susan VanderPlas <srvanderplas@gmail.com>
2013-06-20 18:50:17 -0500

    Improved test files that generate multiple plots and produce one final page which can be used to evaluate any changes to the code for bugs.

--- examples/SimpleLineplot.R
+++ examples/SimpleLineplot.R
@@ -3,18 +3,49 @@ library(ggplot2)
 
 #' Demonstrates line plots in animint, debugging, serves as an example
 
-# Generate data
+#' Generate data
 data <- data.frame(x=rep(1:10, times=5), group = rep(1:5, each=10))
+data$lt <- factor(data$group%%2+1) # linetype
 data$group <- as.factor(data$group)
 data$y <- rnorm(length(data$x), data$x, .5) + rep(rnorm(5, 0, 2), each=10)
 
-# Simple line plot
-p <- ggplot() + geom_line(data=data, aes(x=x, y=y, group=group))
-p
-gg2animint(list(p1 = p), out.dir="./junk", open.browser=FALSE)
+
+#' Simple line plot
+p1 <- ggplot() + geom_line(data=data, aes(x=x, y=y, group=group))
+p1
+# gg2animint(list(p1=p1), out.dir="./junk")
 
 
-# Simple line plot with colors...
-p <- ggplot() + geom_line(data=data, aes(x=x, y=y, colour=group, group=group))
-p
-gg2animint(list(p1 = p), out.dir="./junk", open.browser=FALSE)
+#' Simple line plot with colors...
+p2 <- ggplot() + geom_line(data=data, aes(x=x, y=y, colour=group, group=group))
+p2
+# gg2animint(list(p1=p1, p2=p2), out.dir="./junk")
+
+#' Simple line plot with colors and linetype
+p3 <- ggplot() + geom_line(data=data, aes(x=x, y=y, colour=group, group=group, linetype=lt))
+p3
+# gg2animint(list(p1=p1, p2=p2, p3=p3))
+
+#' Use automatic linetypes from ggplot with coerced factors
+p4 <- ggplot() + geom_line(data=data, aes(x=x, y=y, colour=group, group=group, linetype=factor(group)))
+p4
+# gg2animint(list(p1=p1, p2=p2, p3=p3, p4=p4))
+
+#' Manually specify linetypes using <length, space, length, space...> notation
+data$lt <- rep(c("2423", "2415", "331323", "F2F4", "solid"), each=10)
+p5 <- ggplot() + geom_line(data=data, aes(x=x, y=y, colour=group, group=group, linetype=lt)) + scale_linetype_identity()
+p5
+# gg2animint(list(p1=p1, p2=p2, p3=p3, p4=p4, p5=p5))
+
+#' All possible linetypes
+lts <- scales::linetype_pal()(13)
+lt1 <- data.frame(x=0, xend=.25, y=1:13, yend=1:13, lt=lts)
+p6 <- ggplot()+geom_segment(data=lt1, aes(x=x, xend=xend, y=y, yend=yend, linetype=lt)) + 
+      scale_linetype_identity() + geom_text(data=lt1, aes(x=-.125, y=y, label=lt), hjust=0)
+# gg2animint(list(p1=p1, p2=p2, p3=p3, p4=p4, p5=p5, p6=p6))
+
+lts2 <- c("solid", "dashed", "dotted", "dotdash", "longdash", "twodash")
+lt2 <- data.frame(x=0, xend=.25, y=1:6, yend=1:6, lt=lts2)
+p7 <- ggplot() + geom_segment(data=lt2, aes(x=x, xend=xend, y=y, yend=yend, linetype=lt)) + 
+      scale_linetype_identity() + geom_text(data=lt2, aes(x=-.125, y=y, label=lt), hjust=0)
+gg2animint(list(p1=p1, p2=p2, p3=p3, p4=p4, p5=p5, p6=p6, p7=p7))
--- examples/SimpleScatterplot.R
+++ examples/SimpleScatterplot.R
@@ -5,47 +5,52 @@ library(ggplot2)
 # Randomly generate some data
 data <- data.frame(x=rnorm(100, 50, 15))
 data$y <- with(data, runif(100, x-5, x+5))
-
-p <- qplot(data=data, x=x, y=y, geom="point")
-p
-# This throws an error.
-## TODO: qplot specification should be feasible... how to implement?
-
-# Must use empty ggplot() statement because of structure of ggplot/qplot object
-splot <- ggplot() + geom_point(data=data, aes(x=x, y=y)) #+ geom_smooth(data=data, aes(x=x, y=y))
-
-# Must provide a named list of ggplots.
-gg2animint(list(p1 = ggplot() + geom_point(data=data, aes(x=x, y=y))), out.dir="./junk", open.browser=FALSE)
-
-#' Demonstrates axis -- works with factor data
 data$xnew <- round(data$x/20)*20
 data$xnew <- as.factor(data$xnew)
-
-p <- ggplot() + geom_point(data=data, aes(x=xnew, y=y))
-p
-gg2animint(list(p1 = ggplot() + geom_point(data=data, aes(x=xnew, y=y))), out.dir="./junk", open.browser=FALSE)
-
-
-#' Colors?
 data$class <- factor(round(data$x/10)%%2, labels=c("high", "low"))
 data$class4 <- factor(round(data$x/10)%%4, labels=c("high", "medhigh", "medlow", "low"))
 
-#' Specify colors using R color names does not work
-p <- ggplot() + geom_point(data=data, aes(x=xnew, y=y), colour="blue")
+
+p <- qplot(data=data, x=x, y=y, geom="point")
 p
-gg2animint(list(p1 = ggplot() + geom_point(data=data, aes(x=xnew, y=y), colour="blue", fill="blue")), out.dir="./junk", open.browser=FALSE)
+gg2animint(list(p=p))
+# This throws an error.
+## TODO: qplot specification should be feasible... how to implement?
+
+#' Must use empty ggplot() statement because of structure of ggplot/qplot object
+#' Must provide a named list of ggplots.
+p1 <- ggplot() + geom_point(data=data, aes(x=x,y=y))
+p1
+# gg2animint(list(p1=p1))
+
+#' Demonstrates axis -- works with factor data
+p2 <- ggplot() + geom_point(data=data, aes(x=xnew, y=y))
+p2
+# gg2animint(list(p1=p1, p2=p2))
+
+
+#' Colors
+#' Specify colors using R color names works
+p3 <- ggplot() + geom_point(data=data, aes(x=xnew, y=y), colour="blue")
+p3
+# gg2animint(list(p1=p1, p2=p2, p3=p3))
 
 #' Specify colors manually using hex values works
-p <- ggplot() + geom_point(data=data, aes(x=xnew, y=y, colour=class, fill=class)) + scale_colour_manual(values=c("#FF0000", "#0000FF")) + scale_fill_manual(values=c("#FF0000", "#0000FF"))
-p
-gg2animint(list(p1 = p), out.dir="./junk", open.browser=FALSE)
+p4 <- ggplot() + geom_point(data=data, aes(x=xnew, y=y, colour=class, fill=class)) + scale_colour_manual(values=c("#FF0000", "#0000FF")) + scale_fill_manual(values=c("#FF0000", "#0000FF"))
+p4
+# gg2animint(list(p1=p1, p2=p2, p3=p3, p4=p4))
 
 #' Color by x axis
-p <- ggplot() + geom_point(data=data, aes(x=xnew, y=y, colour=xnew, fill=xnew))
-p
-gg2animint(list(p1 = p), out.dir="./junk", open.browser=FALSE)
+p5 <- ggplot() + geom_point(data=data, aes(x=xnew, y=y, colour=xnew, fill=xnew))
+p5
+# gg2animint(list(p1=p1, p2=p2, p3=p3, p4=p4, p5=p5))
 
 #' Use geom_jitter and color by another variable
-p <- ggplot() + geom_jitter(data=data, aes(x=xnew, y=y, colour=class4, fill=class4))
-p
-gg2animint(list(p1 = p), out.dir="./junk", open.browser=FALSE)
+p6 <- ggplot() + geom_jitter(data=data, aes(x=xnew, y=y, colour=class4, fill=class4))
+p6
+# gg2animint(list(p1=p1, p2=p2, p3=p3, p4=p4, p5=p5, p6=p6))
+
+#' Color by x*y axis (no binning)
+p7 <- ggplot() + geom_point(data=data, aes(x=x, y=y, color=x*y, fill=x*y))
+p7
+gg2animint(list(p1=p1, p2=p2, p3=p3, p4=p4, p5=p5, p6=p6, p7=p7))
