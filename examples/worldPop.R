67626ab9903ae5a7ec7964cf148812c0d4ab764e
Susan VanderPlas <srvanderplas@gmail.com>
2013-07-15 21:00:35 -0500

    Attempt at the stacked barchart (3rd graph on polychart.js)

--- examples/worldPop.R
+++ examples/worldPop.R
@@ -10,19 +10,19 @@ years$population <- 3e6
 ## this should be similar to the example on polychartjs.com
 popPlots <-
   list(bars=ggplot()+
-       geom_bar(aes(x=subcontinent, y=population, showSelected=year),
-                data=worldPop, stat="identity", position="identity")+
-       geom_text(aes(x=subcontinent, y=population,
-                     label=title, showSelected=year),
-                 data=years) +
+         geom_bar(aes(x=subcontinent, y=population, showSelected=year),
+                  data=worldPop, stat="identity", position="identity")+
+         geom_text(aes(x=subcontinent, y=population,
+                       label=title, showSelected=year),
+                   data=years) +
          coord_flip(),
        lines=ggplot()+
-       geom_vline(aes(xintercept=year, clickSelects=year),
-                  data=years, alpha=1/2, size=12)+
-       geom_line(aes(year, population, group=subcontinent),
-                 data=worldPop, alpha=3/4, size=4)+
-       geom_point(aes(year, population, fill=type, colour=type),
-                 data=worldPop))
+         geom_vline(aes(xintercept=year, clickSelects=year),
+                    data=years, alpha=1/2, size=12)+
+         geom_line(aes(year, population, group=subcontinent),
+                   data=worldPop, alpha=3/4, size=4)+
+         geom_point(aes(year, population, fill=type, colour=type),
+                    data=worldPop))
 gg2animint(popPlots)
 
 ## we should at least see the bars in this simpler test.
@@ -50,16 +50,63 @@ gg2animint(list(lines=popPlot,bars=popPlots$bars))
 ## Show the currently selected continent on both plots.
 popPlots2 <-
   list(bars=ggplot()+
-       geom_bar(aes(x=subcontinent, y=population,
-                    showSelected=year, clickSelects=subcontinent),
-                data=worldPop, stat="identity", position="identity")+
-       geom_text(aes(x=subcontinent, y=population,
-                     label=title, showSelected=year),
-                 data=years) +
-       coord_flip(),
+         geom_bar(aes(x=subcontinent, y=population,
+                      showSelected=year, clickSelects=subcontinent),
+                  data=worldPop, stat="identity", position="identity")+
+         geom_text(aes(x=subcontinent, y=population,
+                       label=title, showSelected=year),
+                   data=years) +
+         coord_flip(),
        lines=ggplot()+
-       make_tallrect("year", worldPop)+
-       geom_line(aes(year, population, group=subcontinent,
-                     clickSelects=subcontinent),
-                 data=worldPop, size=4, alpha=3/4))
+         make_tallrect("year", worldPop)+
+         geom_line(aes(year, population, group=subcontinent,
+                       clickSelects=subcontinent),
+                   data=worldPop, size=4, alpha=3/4)+
+         geom_point(aes(year, populations, clickSelects=subcontinent, colour=type),
+                    data=worldPop, size=4, alpha=3/4)+
+         scale_colour_manual(values=c("black", "red")))
 gg2animint(popPlots2)
+
+
+library(plyr)
+popCumSum <- ddply(worldPop[order(worldPop$year, worldPop$subcontinent),], .(year), transform, 
+                   cumPop = cumsum(population)/sum(population), 
+                   cumPop.lower = cumsum(c(0, population[-length(population)]))/sum(population))
+popCumSum$cumCenter = rowMeans(popCumSum[,c("cumPop", "cumPop.lower")])
+
+popPlots3 <-
+  list(bars=ggplot()+
+         geom_bar(aes(x=subcontinent, y=population,
+                      showSelected=year, clickSelects=subcontinent),
+                  data=worldPop, stat="identity", position="identity")+
+         geom_text(aes(x=subcontinent, y=population,
+                       label=title, showSelected=year),
+                   data=years) +
+         coord_flip(),
+       lines=ggplot()+
+         make_tallrect("year", worldPop)+
+         geom_line(aes(year, population, group=subcontinent,
+                       clickSelects=subcontinent),
+                   data=worldPop, size=4, alpha=3/4)+
+         geom_point(aes(year, population, colour=type, 
+                        clickSelects=subcontinent),
+                    data=worldPop, size=4, alpha=.6)+
+         scale_colour_manual(values=c("black", "red")),
+       stack=ggplot()+ 
+         geom_rect(aes(xmin=0, xmax=0.4, ymin=cumPop.lower, ymax=cumPop, fill=factor(subcontinent), 
+                      showSelected=year, clickSelects=subcontinent),
+                  data=popCumSum, colour="#000000")+
+         xlim(c(0, 1))+
+         geom_point(aes(x=.5, y=cumCenter, colour=factor(subcontinent), 
+                        showSelected=year, clickSelects=subcontinent), 
+                    data=popCumSum, size=4)+
+         geom_text(aes(x=.55, y=cumCenter, label=subcontinent, 
+                       showSelected=year, clickSelects=subcontinent), 
+                   data=popCumSum, hjust=0)
+)
+gg2animint(popPlots3)
+
+## TODO: separate bar stacks for different divisions: What's there replicates polycharts.js, 
+## but it's not correct (i.e. N. America and The Americas in the same stack). 
+
+## TODO: figure out how to sort factor order by population for bars?
