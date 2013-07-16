249ea2f07c75129247c46bf9ab2e23396d984c10
Susan VanderPlas <srvanderplas@gmail.com>
2013-07-16 16:57:39 -0500

    proof-of-concept for legend generation.

--- R/legends.R
+++ R/legends.R
@@ -3,17 +3,39 @@ library(animint)
 library(plyr) # to access round_any
 movies$decade <- round_any(movies$year, 10)
 m <- ggplot(movies, aes(x=rating, colour=decade, group=decade)) + 
-  geom_density(fill=NA) + scale_colour_continuous(guide="legend") + 
-  guides(colour=guide_legend(order=1))
+  geom_density(fill=NA) + scale_colour_continuous(guide="legend") 
+
+m <- ggplot(movies, aes(x=rating, colour=decade, group=decade)) + geom_density(fill=NA) + scale_colour_continuous(guide="none")
 m 
 
-temp <- ggplot2::ggplot_build(m)
+mb <- ggplot_build(m)
+aes.scales <- which(sapply(mb$plot$scales$scales, function(i) sum(i$aesthetics%in%c("colour", "size", "fill", "linetype", "alpha"))>0))
 
-temp.tab <- ggplot2::ggplot_gtable(temp)
-grid(temp.tab$grobs[[8]])
-r <- gtable_filter(ggplot_gtable(ggplot_build(m)), "guide-box") 
-gg<- r$grobs[[1]]
 
-gtable_filter(ggplot_gtable(ggplot_build(m)), trim=TRUE, "legend.key.rect") 
 
-ggplot2:::guide_geom.legend(m$guides$colour, m$scales$scales[[1]], m$mapping)
+getLegend <- function(mb, i){
+  sc <- mb$plot$scales$scales[[i]]
+  guidetype <- sc$guide
+  sc.aes <- sc$aesthetics
+  bk <- ggplot2:::scale_breaks(sc)
+  val <- ggplot2:::scale_map(sc, bk)
+  labels <- ggplot2:::scale_labels(sc)
+  if(sc.aes %in% c("colour", "fill")){
+    val <- toRGB(val)
+  }
+  df <- data.frame(breaks = bk, value = val, label = labels)
+  df <- df[which(rowSums(is.na(df))==0),] # return only those entries that have breaks, values, and labels.
+  list(guide = guidetype, 
+       aesthetic = sc.aes, 
+       title = as.character(as.expression(mb$plot$mapping[[sc.aes]])), 
+       legend = df)
+}
+
+legends <- lapply(aes.scales, getLegend, mb=mb)
+
+m <- ggplot(movies, aes(x=length, y=rating, size=votes, colour=factor(Comedy))) + scale_colour_manual(values=c("black", "green")) + geom_jitter(alpha=.5) + scale_size_area() + xlim(c(20, 300))
+m
+mb <- ggplot_build(m)
+aes.scales <- which(sapply(mb$plot$scales$scales, function(i) sum(i$aesthetics%in%c("colour", "size", "fill", "linetype", "alpha"))>0))
+
+legends <- lapply(aes.scales, getLegend, mb=mb)
