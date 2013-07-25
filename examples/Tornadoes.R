b9083579caba3200f8d6274b827311438820724a
Susan VanderPlas <srvanderplas@gmail.com>
2013-07-24 22:16:36 -0500

    Updated and edited Tornado example with cleaned dataset loaded.

--- examples/Tornadoes.R
+++ examples/Tornadoes.R
@@ -4,71 +4,33 @@
 #' Location Codes - http://www.spc.noaa.gov/wcm/loccodes.html
 #' State FIPS Codes - http://www.spc.noaa.gov/wcm/fips_usa.gif
 #' County FIPS Codes - http://www.spc.noaa.gov/wcm/stnindex_all.txt
+#' State/County Area and Population - http://quickfacts.census.gov/qfd/download/DataSet.txt
 #' 
 #' Image Inspiration -  http://www.kulfoto.com/pic/0001/0033/b/h4n5832833.jpg
 library(maps)
 library(plyr)
 library(ggplot2)
 library(animint)
+data(Tornadoes)
+stateOrder <- data.frame(state = unique(UStornadoes$state)[order(unique(UStornadoes$TornadoesSqMile), decreasing=T)], rank = 1:49) # order states by tornadoes per square mile
+UStornadoes$state <- factor(UStornadoes$state, levels=stateOrder$state, ordered=TRUE)
+UStornadoes$weight <- 1/UStornadoes$LandArea
+# useful for stat_bin, etc. 
 
-# url <- "http://www.spc.noaa.gov/wcm/data/1950-2012_torn.csv"
-data <- read.csv("../data/1950-2012_torn.csv", header=FALSE)
-names(data) <- c("ID", "year", "month", "day", "date", "time", "tz", "state", "fips", "state.tnum", "f", "injuries", "fatalities", "propertyLoss", "cropLoss", "startLat", "startLong", "endLat", "endLong", "trackLength", "trackWidth", "numStatesAffected", "stateNumber", "segmentNumber", "FipsCounty1", "FipsCounty2", "FipsCounty3", "FipsCounty4")
+USpolygons <- map_data("state")
+USpolygons$state = state.abb[match(USpolygons$region, tolower(state.name))]
 
-continentalUS <- function(lat, long){
-  apply(cbind(lat>25.2, lat<49.4, long> -124.7, long< -67.1), 1, prod)
-}
-
-data2 <- data
-# if startLat is 0 and endLat is >0 and dist < .2, set startLat = endLat
-idx <- which(data$startLat==0 & data$endLat!=0 & data$trackLength<.2)
-data2$startLat[idx] <- data2$endLat[idx]
-# if startLat is > 0 and endLat is 0 and dist < .2, set endLat = startLat
-idx <- which(data$startLat!=0 & data$endLat==0 & data$trackLength<.2)
-data2$endLat[idx] <- data2$startLat[idx]
-# if startLong is 0 and endLong is >0 and dist < .2, set startLong = endLong
-idx <- which(data$startLong==0 & data$endLong!=0 & data$trackLength<.2)
-data2$startLong[idx] <- data2$endLong[idx]
-# if startLong is > 0 and endLong is 0 and dist < .2, set endLong = startLong
-idx <- which(data$startLong!=0 & data$endLong==0 & data$trackLength<.2)
-data2$endLong[idx] <- data2$startLong[idx]
-
-# continuous-ish time
-data2$time <- data2$year + (data2$month-.5)/12
-save("data2", file="./data/Tornadoes.RData")
-
-load("../data/Tornadoes.RData")
-
-data3 <- subset(data2, continentalUS(startLat, startLong) & continentalUS(endLat, endLong) & f>=0)
-
-states_map <- map_data("state")
-quickfacts <- read.csv("http://quickfacts.census.gov/qfd/download/DataSet.txt")
-quickfacts <- quickfacts[,c(1, 2, 51)]
-names(quickfacts) <- c("FipsCounty", "TotalPop2012", "LandArea")
-state.facts <- quickfacts[which(quickfacts$FipsCounty%%1000==0),]
-state.facts$FipsCounty <- state.facts$FipsCounty/1000 # to conform with tornadoes dataset state FIPS codes
-names(state.facts) <- c("fips", "TotalPop2012", "LandArea")
-
-data3 <- merge(data3, state.facts, all.x=TRUE)
-data3 <- ddply(data3, .(fips, f), transform, TornadoesSqMile = length(LandArea)/LandArea)
-
-states_map$state = state.abb[match(states_map$region, tolower(state.name))]
-
-stateOrder <- data.frame(state = unique(data3$state)[order(unique(data3$TornadoesSqMile), decreasing=T)],
-                         rank = 1:49)
-data3$state <- factor(data3$state, levels=stateOrder$state, ordered=TRUE)
-data3$weight <- 1/data3$LandArea
-
-statemap <- ggplot() + geom_polygon(data=states_map, aes(x=long, y=lat, group=group), fill="black", colour="grey") +
-  geom_segment(data=data3, aes(x=startLong, y=startLat, xend=endLong, yend=endLat, size=trackWidth), colour="#55B1F7", alpha=.2) +
-  geom_segment(data=data3, aes(x=startLong, y=startLat, xend=endLong, yend=endLat, size=trackWidth, alpha=f), colour="#55B1F7") +
+statemap <- ggplot() + geom_polygon(data=USpolygons, aes(x=long, y=lat, group=group), fill="black", colour="grey") +
+  geom_segment(data=UStornadoes, aes(x=startLong, y=startLat, xend=endLong, yend=endLat, size=trackWidth), colour="#55B1F7", alpha=.2) +
+  geom_segment(data=UStornadoes, aes(x=startLong, y=startLat, xend=endLong, yend=endLat, size=trackWidth, alpha=f), colour="#55B1F7") +
   scale_size_continuous("Width (yd)", range=c(.5, 2)) + 
   scale_alpha_continuous("Strength (F or EF scale)", range=c(.3, 1)) + 
   ggtitle("Tornado Paths, 1950-2006")
 
 ## BUG! interactive version... geom_bar + stat_bin does not work!
-USpolygons <- states_map
-UStornadoes <- data3
+USpolygons <- USpolygons
+UStornadoes <- UStornadoes
+
 tornado.bar <- list(map=ggplot()+
      geom_polygon(aes(x=long, y=lat, group=group),
                   data=USpolygons, fill="black", colour="grey") +
@@ -110,31 +72,45 @@ tornado.anim <-
        time=list(variable="year",ms=2000))
 gg2animint(tornado.ts, "tornado-anim")
 
-# Works with geom_freqpoly
-timehist <- ggplot() + geom_freqpoly(data=data3, aes(x=year, group=f, fill=f, colour=f)) + xlab("Time") + ylab("Recorded Tornadoes") + ggtitle("Recorded Tornadoes over Time")
 
-statehist <- ggplot() + geom_freqpoly(data=data3, aes(x=state, group=factor(f), fill=f, colour=f, weight=weight), stat="bin") + xlab("State") + ylab("Tornadoes per Square Mile") + ggtitle("Recorded Tornadoes by State") + coord_flip()
-
-gg2animint(list(mapa=timehist))
-gg2animint(list(mapa=statehist))
-gg2animint(list(mapa=statemap))
-
-
-timehist <- ggplot() + geom_histogram(data=data3, aes(x=year, group=f, fill=f, colour=f)) + xlab("Time") + ylab("Recorded Tornadoes") + ggtitle("Recorded Tornadoes over Time")
-
-statehist <- ggplot() + geom_histogram(data=data3, aes(x=state, group=interaction(f, state), fill=f, colour=f, weight=weight), stat="bin") + xlab("State") + ylab("Tornadoes per Square Mile") + ggtitle("Recorded Tornadoes by State") + coord_flip()
+## how to get gg2animint to play nice with computed stuff... just pass in the computed data without groups specified. So, either user has to compute stuff using ggplot_build or ddply, and then pass into animint, or we figure out how to determine whether group is important or not. Attempt at determining importance is implemented in animint.R - if there are different numbers of rows in plot$layer[[i]]$data and plistextra$data[[i]], this means geom is calculated. If geom is calculated and group overlaps with another aesthetic (color, etc.), then remove group from subord, subvars, and aes.
+# temp <- ggplot2::ggplot_build(statehist)
+# statehist <- ggplot() + geom_rect(data=temp$data[[1]], aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, fill=fill, colour=colour)) + scale_fill_identity() + scale_colour_identity() + scale_x_continuous(breaks=unique(temp$data[[1]]$x), labels=unique(UStornadoes$state)) + coord_flip()
+# gg2animint(list(mapa=statehist, width=list(420), height=list(600)))
 
 
 ## Side comment: I still like the bar plot better than the line plot, since there is not a relationship between subsequent observations, as alphabetic ordering of states doesn't have any geographic meaning. A bar plot is much more informative. 
 
-## how to get gg2animint to play nice with computed stuff... just pass in the computed data without groups specified. So, either user has to compute stuff using ggplot_build or ddply, and then pass into animint, or we figure out how to determine whether group is important or not. Attempt at determining importance is implemented in animint.R - if group is a one-to-one function of line number, then remove group from subord, subvars, and aes.
-# temp <- ggplot2::ggplot_build(statehist)
-# statehist <- ggplot() + geom_rect(data=temp$data[[1]], aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, fill=fill, colour=colour)) + scale_fill_identity() + scale_colour_identity() + scale_x_continuous(breaks=unique(temp$data[[1]]$x), labels=unique(data3$state)) + coord_flip()
-# gg2animint(list(mapa=statehist, width=list(420), height=list(600)))
+timehist <- ggplot() + geom_histogram(data=UStornadoes, aes(x=year, group=f, fill=f, colour=f, clickSelects=f)) + xlab("Time") + ylab("Recorded Tornadoes") + ggtitle("Recorded Tornadoes over Time")
 
-gg2animint(list(mapa=timehist))
+statehist <- ggplot() + geom_histogram(data=UStornadoes, aes(x=state, fill=f, group = f, colour=f, showSelected=f, weight=weight), stat="bin", position="identity") + xlab("State") + ylab("Tornadoes per Square Mile") + ggtitle("Recorded Tornadoes by State") + coord_flip()
+
+gg2animint(list(p1=timehist, p2=statehist, width=list(500), height=list(500)))
+# while it would be nice to select state and have tornadoes shown over time, we would have to use group = interaction(f,state) or something to that effect, which would not work as the group aesthetic must be shared by at least one other scalable aesthetic. 
+
+timehist <- ggplot() + geom_histogram(data=UStornadoes, aes(x=year, group=f, fill=f, colour=f, clickSelects=f)) + xlab("Time") + ylab("Recorded Tornadoes") + ggtitle("Recorded Tornadoes over Time")
+
+statehist <- ggplot() + geom_histogram(data=UStornadoes, aes(x=state, fill=f, group = f, colour=f, showSelected=f, clickSelects=state, weight=weight), stat="bin", position="identity") + xlab("State") + ylab("Tornadoes per Square Mile") + ggtitle("Recorded Tornadoes by State") + coord_flip()
+
+statebyyear <- ddply(UStornadoes, .(year, state), summarise, count=length(f))
+
+statetimeline <- ggplot() + geom_line(data=statebyyear, aes(x=year, y=count, group=state, showSelected=state))
+
+gg2animint(list(p1=timehist, p2=statehist, p3=statetimeline, width=list(500), height=list(500)))
+## Can't handle two clickSelects floating around...
 
 
-gg2animint(list(statemap = statemap, timehist = statehist,
-                width=list(statemap=1000, timehist=500),  
-                height=list(statemap=420, timehist=420)))
+timehist <- ggplot() + geom_histogram(data=UStornadoes, aes(x=year, group=f, fill=f, colour=f)) + xlab("Time") + ylab("Recorded Tornadoes") + ggtitle("Recorded Tornadoes over Time")
+
+statehist <- ggplot() + geom_histogram(data=UStornadoes, aes(x=state, fill=f, group = f, colour=f, clickSelects=state, weight=weight), stat="bin", position="stack") + xlab("State") + ylab("Tornadoes per Square Mile") + ggtitle("Recorded Tornadoes by State") + coord_flip()
+# clickSelects won't work unless group=state, but then stat_bin doesn't work. :(
+
+statebyyear <- ddply(UStornadoes, .(year, state), summarise, count=length(f), f = mean(f))
+
+statetimeline <- ggplot() + geom_line(data=statebyyear, aes(x=year, y=count, group = state, colour=f, showSelected=state))
+
+gg2animint(list(p1=timehist, p2=statehist, p3=statetimeline, width=list(500), height=list(500)))
+
+
+
+
