64851a4185090c010be11e7a2aa88fa97ea09a35
Toby Dylan Hocking <toby@sg.cs.titech.ac.jp>
2013-10-22 17:11:53 +0900

    export gg2animint_knitr

--- R/knitr.R
+++ R/knitr.R
@@ -1,3 +1,10 @@
+##' Insert an interactive animation into an R markdown document.
+##' @param plot.list named list of ggplots and option lists to pass to
+##' gg2animint.
+##' @return Nothing, but outputs HTML that will render the interactive
+##' animation.
+##' @author Toby Dylan Hocking
+##' @export
 gg2animint_knitr <- function(plot.list){
   gg2animint(plot.list, ".", FALSE)
   html <- system.file(file.path("htmljs","scripts.html"), package="animint")
