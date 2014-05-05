e58c8b6675dd8465fe4816168053d0e18fd6f228
Toby Dylan Hocking <tdhock5@gmail.com>
2014-05-05 17:39:06 -0400

    WorldBank good and bad animated transitions

--- examples/WorldBank.R
+++ examples/WorldBank.R
@@ -7,41 +7,22 @@ data(WorldBank)
 ##                      colorvar="region", sizevar="population",
 ##                      options=list(width=700, height=600))
 
-## A google motion chart is just a scatterplot with size and color
-## that moves over time.
-pop.range <- range(WorldBank$pop,na.rm=TRUE)
-log.scale <- seq(ceiling(log10(pop.range[1])),floor(log10(pop.range[2])),by=1)
-pop.breaks <- sort(c(10^log.scale))
-motion <-
+## This example is BAD because it does not specify a key variable, so
+## the animated transitions result in points flying over the
+## scatterplot, which is not good for understand the evolution over
+## time of these countries' data.
+not.na <- subset(WorldBank, !(is.na(life.expectancy) | is.na(fertility.rate)))
+subset(not.na, is.na(not.na$population))
+not.na[not.na$country=="Kuwait", "population"] <- 1700000
+bad <-
   list(scatter=ggplot()+
        geom_point(aes(life.expectancy, fertility.rate, clickSelects=country,
                       showSelected=year, colour=region, size=population),
-                  data=WorldBank)+
-       ##scale_size_area(max_size=40)+
-       make_text(WorldBank, 55, 9, "year"),
-       timeSeries=ggplot()+
-       make_tallrect(WorldBank, "year")+
-       geom_line(aes(year, life.expectancy, group=country,
-                     clickSelects=country),
-                 data=WorldBank, size=4, alpha=3/5),
-       time=list(variable="year",ms=3000),
-       duration=list(year=1000))
-gg2animint(motion, "motion")
-
-## Why can't we use range with scale_size_area? Because the number of
-## pixels should be proportional to the number represented i.e. it is
-## a linear, not affine function.
-motion.area <-
-  list(scatter=ggplot()+
-       geom_point(aes(life.expectancy, fertility.rate, clickSelects=country,
-                      showSelected=year, colour=region, size=population),
-                  data=WorldBank)+
+                  data=not.na)+
        geom_text(aes(life.expectancy, fertility.rate, label=country,
                      showSelected=country, showSelected2=year),
-                 data=WorldBank)+
-       continuous_scale("size","area",palette=function(x){
-         scales:::rescale(sqrt(abs(x)), c(2,20), c(0,1))
-       },breaks=10^(4:9))+
+                 data=not.na)+
+       scale_size_animint(breaks=10^(5:9))+
        make_text(WorldBank, 55, 9, "year"),
        ts=ggplot()+
        make_tallrect(WorldBank, "year")+
@@ -50,10 +31,39 @@ motion.area <-
                  data=WorldBank, size=4, alpha=3/5),
        time=list(variable="year",ms=3000),
        bar=ggplot()+
+       theme_animint(height=2400)+
        geom_bar(aes(country, life.expectancy, fill=region,
                     showSelected=year, clickSelects=country),
                 data=WorldBank, stat="identity", position="identity")+
        coord_flip(),
-       duration=list(year=1000),
-       height=list(bar=1500))
-gg2animint(motion.area, "motion-area")
+       duration=list(year=1000))
+gg2animint(bad, "WorldBank-bad")
+
+## This example is good because it uses constancy
+## http://bost.ocks.org/mike/constancy/
+good <-
+  list(scatter=ggplot()+
+       geom_point(aes(life.expectancy, fertility.rate, clickSelects=country,
+                      showSelected=year, colour=region, size=population,
+                      key=country), # key aesthetic for animated transitions!
+                  data=not.na)+
+       geom_text(aes(life.expectancy, fertility.rate, label=country,
+                     showSelected=country, showSelected2=year,
+                     key=country), #also use key here!
+                 data=not.na)+
+       scale_size_animint(breaks=10^(5:9))+
+       make_text(WorldBank, 55, 9, "year"),
+       ts=ggplot()+
+       make_tallrect(WorldBank, "year")+
+       geom_line(aes(year, life.expectancy, group=country, colour=region,
+                     clickSelects=country),
+                 data=WorldBank, size=4, alpha=3/5),
+       time=list(variable="year",ms=3000),
+       bar=ggplot()+
+       theme_animint(height=2400)+
+       geom_bar(aes(country, life.expectancy, fill=region,
+                    showSelected=year, clickSelects=country),
+                data=WorldBank, stat="identity", position="identity")+
+       coord_flip(),
+       duration=list(year=1000))
+gg2animint(good, "WorldBank-good")
