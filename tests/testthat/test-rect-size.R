b13834b12a744792007dedb4bd5555dd92958a71
Anonymous@example.com <cpsievert1@gmail.com>
2014-05-21 16:13:34 -0500

    Whoops; add tobys suggested changes to test-rect-size

--- tests/testthat/test-rect-size.R
+++ tests/testthat/test-rect-size.R
@@ -14,36 +14,33 @@ df <- data.frame(xmin = c(0, 0),
                  ymax = c(1, 3),
                  size = c(0, 5))
 
-viz1 <- list(segs = ggplot() + geom_rect(data = df, size = 5, color = "violet",
-                    aes(xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax)))
-viz2 <- list(segs = ggplot() + geom_rect(data = df, size = 0, color = "violet",
-                    aes(xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax)))
-viz3 <- list(segs = ggplot() + geom_rect(data = df, color = "violet",
-                    aes(xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax, size = size)) +
-                    scale_size_identity())
-info1 <- animint2HTML(viz1, context, "size5")
-info2 <- animint2HTML(viz2, context, "size0")
-info3 <- animint2HTML(viz3, context, "size") #suppressWarning?
-html1 <- parse_page(info1)
-html2 <- parse_page(info2)
-html3 <- parse_page(info3)
-
 test_that("rect size translates to stroke-width", {
-  style1 <- getStyle(html1)
-  expect_equal(style1[1], "opacity: 1; stroke-dasharray: 0; stroke-width: 5; fill: violet; stroke: violet;")
-  expect_equal(style1[2], "opacity: 1; stroke-dasharray: 0; stroke-width: 5; fill: violet; stroke: violet;")
+  viz <- list(segs = ggplot() + geom_rect(data = df, size = 5, color = "violet",
+                                           aes(xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax)))
+  info <- animint2HTML(viz, context, "size5")
+  html <- parse_page(info)
+  style <- getStyle(html)
+  expect_equal(style[1], "opacity: 1; stroke-dasharray: 0; stroke-width: 5; fill: violet; stroke: violet;")
+  expect_equal(style[2], "opacity: 1; stroke-dasharray: 0; stroke-width: 5; fill: violet; stroke: violet;")
 })
 
 test_that("zero rect size translates to stroke-width", {
-  
-  style2 <- getStyle(html2)
-  expect_equal(style2[1], "opacity: 1; stroke-dasharray: 0; stroke-width: 0; fill: violet; stroke: violet;")
-  expect_equal(style2[2], "opacity: 1; stroke-dasharray: 0; stroke-width: 0; fill: violet; stroke: violet;")
+  viz <- list(segs = ggplot() + geom_rect(data = df, size = 0, color = "violet",
+                                          aes(xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax)))
+  info <- animint2HTML(viz, context, "size0")
+  html <- parse_page(info)
+  style <- getStyle(html)
+  expect_equal(style[1], "opacity: 1; stroke-dasharray: 0; stroke-width: 0; fill: violet; stroke: violet;")
+  expect_equal(style[2], "opacity: 1; stroke-dasharray: 0; stroke-width: 0; fill: violet; stroke: violet;")
 })
 
 test_that("rect size range translates to stroke-width", {
-  
-  style3 <- getStyle(html3)
-  expect_equal(style3, c("opacity: 1; stroke-dasharray: 0; stroke-width: 0; fill: violet; stroke: violet;",
+  viz <- list(segs = ggplot() + geom_rect(data = df, color = "violet",
+                 aes(xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax, size = size)) +
+                 scale_size_identity())
+  info <- animint2HTML(viz, context, "size") #suppressWarning?
+  html <- parse_page(info)
+  style <- getStyle(html)
+  expect_equal(style, c("opacity: 1; stroke-dasharray: 0; stroke-width: 0; fill: violet; stroke: violet;",
                          "opacity: 1; stroke-dasharray: 0; stroke-width: 5; fill: violet; stroke: violet;"))
 })
