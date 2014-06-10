63ae0a23bc761d60e90c97a8007a24bc317f6533
Toby Dylan Hocking <tdhock5@gmail.com>
2014-06-10 16:48:58 -0400

    test color legend

--- tests/testthat/test-point-fill-numeric.R
+++ tests/testthat/test-point-fill-numeric.R
@@ -8,10 +8,16 @@ viz <-
        size=ggplot()+
        geom_point(aes(Sepal.Width, Petal.Width,
                       size=Sepal.Length),
-                  data=iris, color="black", fill="red", pch=21))
+                  data=iris, color="black", fill="red", pch=21),
+       color=ggplot()+
+       geom_point(aes(Sepal.Width, Petal.Width,
+                      color=Petal.Length),
+                  data=iris, size=5)+
+       scale_color_continuous("petal length"))
 
 test_that("legends are produced", {
   info <- animint2dir(viz, open.browser=FALSE)
   expect_identical(names(info$plots$fill$legend), "Petal.Length")
   expect_identical(names(info$plots$size$legend), "Sepal.Length")
+  expect_identical(names(info$plots$color$legend), "petal length")
 })
