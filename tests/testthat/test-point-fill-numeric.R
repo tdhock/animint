dcc73296b89cb6c7fcf136b95eb78e2bef05a68d
Toby Dylan Hocking <tdhock5@gmail.com>
2014-06-10 16:33:20 -0400

    test geom_point(aes(fill|size))

---
+++ tests/testthat/test-point-fill-numeric.R
@@ -0,0 +1,17 @@
+context("geom_point(aes(fill=numeric))")
+
+viz <-
+  list(fill=ggplot()+
+       geom_point(aes(Sepal.Width, Petal.Width,
+                      fill=Petal.Length),
+                  data=iris, color="black", size=5, pch=21),
+       size=ggplot()+
+       geom_point(aes(Sepal.Width, Petal.Width,
+                      size=Sepal.Length),
+                  data=iris, color="black", fill="red", pch=21))
+
+test_that("legends are produced", {
+  info <- animint2dir(viz, open.browser=FALSE)
+  expect_identical(names(info$plots$fill$legend), "Petal.Length")
+  expect_identical(names(info$plots$fill$legend), "Sepal.Length")
+})
