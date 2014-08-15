95b007497c64ef5f88e3a032981e30121321de3e
Toby Dylan Hocking <tdhock5@gmail.com>
2014-08-15 09:33:18 -0400

    test number of axes instead of number of pixels in translate

--- tests/testthat/test-facet-space.R
+++ tests/testthat/test-facet-space.R
@@ -1,15 +1,16 @@
 context("facet-space")
 
-p <- ggplot(mtcars, aes(mpg, wt)) + 
+no.panels <- ggplot(mtcars, aes(mpg, wt)) + 
   geom_point(colour='grey50', size = 4) + 
   geom_point(aes(colour = cyl)) 
 
 test_that("space = 'free' argument in facet_grid works", {
-  viz <- p + facet_grid(.~am, space = "free", scales = "free")
-  info <- animint2HTML(list(plot = viz))
-  nodes <- getNodeSet(info$html, "//g[@id='yaxis']")
-  translate1 <- xmlAttrs(nodes[[1]])["transform"]
-  translate2 <- xmlAttrs(nodes[[2]])["transform"]
-  expect_transform(translate1, c(28, 0))
-  expect_transform(translate2, c(198, 0))
+  panels <- no.panels + facet_grid(.~am, space = "free", scales = "free")
+  viz <- list(gridPlot = panels)
+  info <- animint2HTML(viz)
+  ## Check that there are two x axes and 1 y axis.
+  x.axes <- getNodeSet(info$html, "//g[@id='xaxis']")
+  expect_equal(length(x.axes), 2)
+  y.axes <- getNodeSet(info$html, "//g[@id='xaxis']")
+  expect_equal(length(y.axes), 1)
 })
