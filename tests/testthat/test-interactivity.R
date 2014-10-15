28dc4ac8b6e0803669818fd3f2d253481fb3702c
Anonymous@example.com <cpsievert1@gmail.com>
2014-10-15 15:10:28 -0500

    Test randomly fails

--- tests/testthat/test-interactivity.R
+++ tests/testthat/test-interactivity.R
@@ -73,7 +73,7 @@ clickHTML <- function(...){
 }  
 
 test_that("clickSelects 300 makes 300 <circle> elements", {
-  # randomly fails
+    # randomly fails
   html <- clickHTML(id=300)
   nodes <- getNodeSet(html, '//g[@class="geom1_point_signal"]//circle')
   expect_equal(length(nodes), 300)
