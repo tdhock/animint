a64608b573a73d00d096204fa0a04832efc62bbd
Anonymous@example.com <cpsievert1@gmail.com>
2014-10-14 23:37:18 -0500

    Try setting R_TESTS variable

--- tests/testthat/test-shiny.R
+++ tests/testthat/test-shiny.R
@@ -1,11 +1,13 @@
 context("shiny")
 
+Sys.setenv("R_TESTS" = "") # As suggested by hadley -- https://github.com/hadley/testthat/issues/144
+
 #just in case, try to kill process that we may have started
 killservr <- 'pkill -f "shiny::runApp\\(port=6012"'
 system(killservr)
 # serve the app using a seperate R session so we can execute subsequent commands
 cmd <- "R -e \'shiny::runApp(port=6012, appDir=system.file(\"examples/shiny\", package = \"animint\"))\'"
-system(cmd, intern = FALSE, wait = FALSE)
+system(cmd, wait = FALSE)
 
 
 test_that("animint plot renders in a shiny app", {
@@ -26,7 +28,7 @@ system(killrmd)
 
 # serve the "app" using a seperate R session so we can execute subsequent commands
 rmd <- "R -e \'rmarkdown::run(shiny_args=list(port=6014), system.file(\"examples/rmarkdown/index.Rmd\", package = \"animint\"))\'"
-system(rmd, intern = FALSE, wait = FALSE)
+system(rmd, wait = FALSE)
 
 test_that("animint plot renders in an interactive document", {
   Sys.sleep(5) # give shiny a second to do it's thing
