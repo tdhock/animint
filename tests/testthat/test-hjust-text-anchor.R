4999799a50ce0293d8c3895cd0324acd3c4074f6
Toby Dylan Hocking <tdhock5@gmail.com>
2015-03-10 12:21:23 -0400

    skeleton for hjust tests

---
+++ tests/testthat/test-hjust-text-anchor.R
@@ -0,0 +1,14 @@
+context("hjust text anchor")
+
+test_that('geom_text(hjust=0) => <text style="text-anchor: start">', {
+})
+
+test_that('geom_text(hjust=1) => <text style="text-anchor: end">', {
+})
+
+test_that('geom_text(hjust=0.5) => <text style="text-anchor: middle">', {
+})
+
+test_that('geom_text(hjust=other) => unsupported warning', {
+})
+
