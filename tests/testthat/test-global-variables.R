c00d0284a05efcc2beb82d79df038b4ebd8c0552
Toby Dylan Hocking <tdhock5@gmail.com>
2015-05-06 17:15:39 -0400

    test global variables

---
+++ tests/testthat/test-global-variables.R
@@ -0,0 +1,29 @@
+context("global variables")
+
+viz <- list(scatter=ggplot()+
+  geom_point(aes(y=Petal.Length, x=Sepal.Length,
+                color=Species),
+            data=iris))
+
+myScript <- 'myArray = [];
+for(var b in window) { 
+  if(window.hasOwnProperty(b)) {myArray.push(b);} 
+}
+return myArray;'
+
+getVariables <- function(){
+  remDr$executeScript(myScript)[[1]]
+}
+
+test_that("animint.js only defines 1 object, called animint", {
+  info <- animint2HTML(viz)
+  animint.vars <- getVariables()
+  index.file <- "animint-htmltest/index.html"
+  html.lines <- readLines(index.file)
+  html.without <- html.lines[!grepl("animint.js", html.lines)]
+  cat(html.without, file=index.file, sep="\n")
+  remDr$refresh()
+  without.vars <- getVariables()
+  diff.vars <- animint.vars[!animint.vars %in% without.vars]
+  expect_identical(diff.vars, "animint")
+})
