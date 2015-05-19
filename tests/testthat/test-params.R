b241890bedd245176eae03e0a0d1f31caa2edae8
Toby Dylan Hocking <tdhock5@gmail.com>
2015-05-19 16:52:28 -0400

    animint::toRGB

--- tests/testthat/test-params.R
+++ tests/testthat/test-params.R
@@ -20,7 +20,7 @@ test_that("color is converted to RGB colour", {
 
   expect_equal(length(info$geoms), 1)
   g <- info$geoms[[1]]
-  expected.colour <- as.character(toRGB("grey50"))
+  expected.colour <- as.character(animint::toRGB("grey50"))
   expect_identical(g$params$colour, expected.colour)
   
   node.list <- getNodeSet(info$html, '//g[@class="geom1_step_step"]//path')
@@ -33,12 +33,14 @@ test_that("color is converted to RGB colour", {
   expect_identical(style.vec[["fill"]], "none")
   expect_match(style.vec[["stroke-width"]], "3")
   stroke <- style.vec[["stroke"]]
+  print(stroke)
+  print(expected.colour)
   if(grepl("rgb", stroke)){
     expected.regex <- paste(col2rgb(expected.colour), collapse=", *")
+    print(expected.regex)
     expect_match(stroke, expected.regex)
     ## On firefox, stroke is "rgb(127, 127, 127)"
   }else{
-    print(stroke)
     expect_identical(stroke, expected.colour)
     ## On phantomjs, stroke is "#7f7f7f"
   }
