55c6d201d920942bbf5e138e6e0093b5dcc017c2
Toby Dylan Hocking <tdhock5@gmail.com>
2015-05-26 12:37:21 -0400

    delete WorldBank

--- tests/testthat/test-chunks.R
+++ tests/testthat/test-chunks.R
@@ -41,41 +41,6 @@ test_that("produce informative errors for bad chunk_vars", {
            "use chunk_vars=character() to specify 1 chunk"), fixed=TRUE)
 })
 
-data(WorldBank)
-
-not.na <- subset(WorldBank, !(is.na(life.expectancy) | is.na(fertility.rate)))
-subset(not.na, is.na(not.na$population))
-not.na[not.na$country=="Kuwait", "population"] <- 1700000
-
-good <-
-  list(scatter=ggplot()+
-       geom_point(aes(life.expectancy, fertility.rate, clickSelects=country,
-                      showSelected=year, colour=region, size=population,
-                      tooltip=paste(country, "population", population),
-                      key=country), # key aesthetic for animated transitions!
-                  data=not.na)+
-       geom_text(aes(life.expectancy, fertility.rate, label=country,
-                     showSelected=country, showSelected2=year,
-                     key=country), #also use key here!
-                 data=not.na)+
-       scale_size_animint(breaks=10^(5:9))+
-       make_text(WorldBank, 55, 9, "year"),
-       ts=ggplot()+
-       make_tallrect(WorldBank, "year")+
-       geom_line(aes(year, life.expectancy, group=country, colour=region,
-                     clickSelects=country),
-                 data=WorldBank, size=4, alpha=3/5),
-       time=list(variable="year",ms=3000),
-       bar=ggplot()+
-       theme_animint(height=2400)+
-       geom_bar(aes(country, life.expectancy, fill=region,
-                    showSelected=year, clickSelects=country),
-                data=WorldBank, stat="identity", position="identity")+
-       coord_flip(),
-       duration=list(year=1000),
-       first=list(year=1975, country="United States"),
-       title="World Bank data (single selection)")
-
 data(breakpoints)
 only.error <- subset(breakpoints$error,type=="E")
 only.segments <- subset(only.error,bases.per.probe==bases.per.probe[1])
