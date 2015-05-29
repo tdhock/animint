f7ebcf1477e4e5cde55a493c83c87b13bba6bcb1
kferris10 <kevin.ferris10@gmail.com>
2015-05-29 17:32:03 -0600

    Add stroke and stroke-width attributes to background rectangle

--- inst/htmljs/animint.js
+++ inst/htmljs/animint.js
@@ -513,7 +513,9 @@ var animint = function (to_select, json_file) {
         .attr("y", plotdim.ystart)
         .attr("width", plotdim.xend - plotdim.xstart)
         .attr("height", plotdim.yend - plotdim.ystart)
-        .attr("fill", p_info.panel_background.fill);
+        .attr("fill", p_info.panel_background.fill)
+        .attr("stroke", p_info.panel_background.colour)
+        .attr("stroke-width", p_info.panel_background.size);
 
     } //end of for loop
 
