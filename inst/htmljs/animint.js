1865be536c3dd452e608b07cd174311c745615c1
kferris10 <kevin.ferris10@gmail.com>
2015-05-29 21:43:57 -0600

    Switch to using style instead of attr for background rectangles

--- inst/htmljs/animint.js
+++ inst/htmljs/animint.js
@@ -517,9 +517,9 @@ var animint = function (to_select, json_file) {
         .attr("y", plotdim.ystart)
         .attr("width", plotdim.xend - plotdim.xstart)
         .attr("height", plotdim.yend - plotdim.ystart)
-        .attr("fill", p_info.panel_background.fill)
-        .attr("stroke", p_info.panel_background.colour)
-        .attr("stroke-dasharray", function() {
+        .style("fill", p_info.panel_background.fill)
+        .style("stroke", p_info.panel_background.colour)
+        .style("stroke-dasharray", function() {
           return linetypesize2dasharray(p_info.panel_background.linetype,
                                         p_info.panel_background.size);
         });
