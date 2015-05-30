051898ae497a7c3fa369380c5fd7cf81f3434a6e
kferris10 <kevin.ferris10@gmail.com>
2015-05-29 21:38:33 -0600

    Update pirates example

--- inst/examples/pirates.R
+++ inst/examples/pirates.R
@@ -82,7 +82,9 @@ p_df2 <- p_df %>%
 
 ## generating tiles
 ## each year, I will count the number of attacks in each tile
-bin_df <- bin_2d(p_df$coords.x1, p_df$coords.x2, 60, 30) %>% tbl_df()
+bin_df <- bin_2d(p_df$coords.x1, p_df$coords.x2, 60, 30) %>% 
+  tbl_df() %>% 
+  mutate(id = 1:nrow(.)) ## adding a unique id for each bin
 
 # counting attacks in each tile in each year
 d <- p_df %>% 
@@ -90,8 +92,8 @@ d <- p_df %>%
   group_by(date) %>% 
   do(count = count_bins(bin_df, .$coords.x1, .$coords.x2))
 
-## merging dates and counts
 ## should figure out how to do this with dplyr
+# merging dates and counts
 l <- list()
 for(i in 1:nrow(d)) {
   l[[i]] <- d$count[[i]]
@@ -105,7 +107,16 @@ p_df3 <- l %>%
   group_by(xmid, ymid) %>% 
   mutate(attacks = cumsum(count)) %>% 
   ungroup() %>% 
-  filter(attacks > 0)
+  filter(attacks > 0) %>% 
+  # join with bin_df to add an id column
+  inner_join(select(bin_df, xmid, ymid, id))
+
+# ids for each tile
+p_df4 <- p_df3 %>% 
+  arrange(id, date) %>% 
+  group_by(id) %>% 
+  summarise(text_loc_x = last(date), 
+            text_loc_y = last(log(attacks)))
 
 # animint plots -----------------------------------------
 
@@ -113,52 +124,71 @@ p_df3 <- l %>%
 p_time <- ggplot() + 
   geom_line(aes(date, total_attacks), data = p_df2) + 
   make_tallrect(p_df2, "date") + 
-  labs(y = "Total Attacks", x = "Date") + 
-  theme_animint(width = 450)
+  labs(y = "Total Attacks", x = "Date", 
+       title = "Pirate Attacks from 1995 to 2013") + 
+  theme_animint(width = 550, height = 350)
 
 # points on world map
 p_points <- ggplot() + 
-  geom_rect(aes(xmin = -180, xmax = 192, ymin = -60, ymax = 84), 
-            fill = "lightblue", size = I(.3)) + 
+  #   geom_rect(aes(xmin = -180, xmax = 192, ymin = -60, ymax = 84), 
+  #             fill = "lightblue", size = I(.3)) + 
   geom_polygon(aes(long, lat, group = group), size = I(1), 
                data = countries, fill = "lightgrey", colour = "darkgreen") +
   geom_point(aes(coords.x1, coords.x2, showSelected = date), 
              size = 3, alpha = I(.5), data = p_df) + 
   make_text(p_df, 0, 90, "date", "Pirate Attacks in %d") + 
-  theme(axis.line=element_blank(), axis.text=element_blank(), 
+  theme(panel.background = element_rect(fill = "lightblue"), 
+        axis.line=element_blank(), axis.text=element_blank(), 
         axis.ticks=element_blank(), axis.title=element_blank(), 
         panel.grid.major=element_blank(), panel.grid.minor=element_blank()) + 
-  theme_animint(width = 800)
+  theme_animint(width = 600, height = 350)
 
 # tiles on world map
 p_tiles <- ggplot() + 
-  geom_rect(aes(xmin = -180, xmax = 192, ymin = -60, ymax = 84), 
-            fill = "lightblue", size = I(.3)) + 
+  #   geom_rect(aes(xmin = -180, xmax = 192, ymin = -60, ymax = 84), 
+  #             fill = "lightblue", size = I(.3)) + 
   geom_polygon(aes(long, lat, group = group), size = I(1), 
                data = countries, fill = "lightgrey", colour = "darkgreen") +
   geom_tile(aes(xmid, ymid, fill = log(attacks), 
                 clickSelects = id, showSelected = date), 
             data = p_df3, colour = I("red")) + 
+  geom_text(aes(xmid, ymid, label = id, 
+                showSelected = id), 
+            data = p_df3) + 
   make_text(p_df, 0, 90, "date", "Pirate Attacks from 1995 to %d") + 
   scale_fill_gradient(low = "#fee5d9", high = "#a50f15", name = "Attacks", 
                       labels = c(1, 10, 50, 400), 
                       breaks = log(c(1, 10, 50, 400))) + 
-  theme(axis.line=element_blank(), axis.text=element_blank(), 
+  theme(panel.background = element_rect(fill = "lightblue"), 
+        axis.line=element_blank(), axis.text=element_blank(), 
         axis.ticks=element_blank(), axis.title=element_blank(), 
         panel.grid.major=element_blank(), panel.grid.minor=element_blank()) + 
-  theme_animint(width = 800)
+  theme_animint(width = 600, height = 350)
 
 # tiles over time
 p_time2 <- ggplot() + 
-  geom_line(aes(date, log(attacks), group = id, showSelected = id), 
-            data = p_df3)
+  make_tallrect(p_df2, "date") + 
+  geom_line(aes(date, log(attacks), group = id, 
+                clickSelects = id, showSelected = id), 
+            data = p_df3) + 
+  geom_text(aes(text_loc_x, text_loc_y, label = id, 
+                clickSelects = id, showSelected = id), 
+            colour = "red", data = p_df4) + 
+  scale_y_continuous(labels = c(1, 7, 55, 400), name = "Total Attacks") + 
+  scale_x_continuous(breaks = c(1995, 2000, 2005, 2010), name = "Date", limits = c(1995, 2013)) + 
+  theme_animint(height = 350, width = 550) + 
+  ggtitle("Attacks in Individual Tiles")
 
 # passing to animint
 ani_list <- list( 
   points = p_points, 
-  timeTotal = p_time,
-  tileTotal = p_tiles, 
-  time = list(variable = "date", ms = 300)
+  tiles = p_tiles, 
+  total = p_time, 
+  tileTotal = p_time2,  
+  time = list(variable = "date", ms = 300), 
+  selector.types = list(id = "multiple"),
+  first = list(id = 767), 
+  title = ""
 )
 animint2dir(ani_list, "pirates_viz", open.browser = FALSE)
 servr::httd("pirates_viz")
