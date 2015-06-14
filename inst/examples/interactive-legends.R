0e491ad454db3d196c866e31376191d2e40b0ef8
kferris10 <kevin.ferris10@gmail.com>
2015-06-14 15:57:52 -0600

    Update interactive legends example to illustrate a potential bug.

--- inst/examples/interactive-legends.R
+++ inst/examples/interactive-legends.R
@@ -4,22 +4,16 @@ library(animint)
 
 p1 <- ggplot() + 
   geom_point(aes(Sepal.Length, Sepal.Width, 
-                 colour = Species, size = Species), 
+                 colour = Species, size = Species, clickSelects = Species), 
              data = iris) + 
-  theme(panel.background = element_rect(fill = "lightblue"), 
-        panel.border = element_rect(fill = NA, 
-                                    color = "black", 
-                                    size = 2, 
-                                    linetype = "dashed"), 
-        panel.margin = grid::unit(.1, "cm")) + 
+  ggtitle("Sepal Data") + 
   facet_wrap(~Species, nrow = 2)
 p2 <- ggplot() + 
   geom_point(aes(Petal.Length, Petal.Width, 
-                 colour = Species, size = Species
+                 colour = Species, size = Species, showSelected = Species
   ), 
   data = iris) + 
-  ggtitle("Petal Data") + 
-  theme_bw()
+  ggtitle("Petal Data")
 viz <- list(sepal = p1, 
             petal = p2, 
             title = "Different Panel Styles")
