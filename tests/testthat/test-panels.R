71c51ff1e42b9fc1463138a34ccab2938095999d
kferris10 <kevin.ferris10@gmail.com>
2015-06-17 09:27:49 -0600

    Add test for no grid lines

--- tests/testthat/test-panels.R
+++ tests/testthat/test-panels.R
@@ -1,19 +1,19 @@
 context("Panel background")
 
-p1 <- ggplot() + 
-  geom_point(aes(Sepal.Length, Sepal.Width, 
-                 colour = Species, size = Species), data = iris) + 
-  theme(panel.background = element_rect(fill = "lightblue"), 
-        panel.border = element_rect(fill = NA, 
-                                    color = "black", 
-                                    size = 2, 
-                                    linetype = "dashed"), 
-        panel.margin = grid::unit(.1, "cm")) + 
+p1 <- ggplot() +
+  geom_point(aes(Sepal.Length, Sepal.Width,
+                 colour = Species, size = Species), data = iris) +
+  theme(panel.background = element_rect(fill = "lightblue"),
+        panel.border = element_rect(fill = NA,
+                                    color = "black",
+                                    size = 2,
+                                    linetype = "dashed"),
+        panel.margin = grid::unit(.1, "cm")) +
   facet_wrap(~Species, nrow = 2)
-p2 <- ggplot() + 
-  geom_point(aes(Petal.Length, Petal.Width, 
-                 colour = Species, size = Species), data = iris) + 
-  ggtitle("Petal Data") + 
+p2 <- ggplot() +
+  geom_point(aes(Petal.Length, Petal.Width,
+                 colour = Species, size = Species), data = iris) +
+  ggtitle("Petal Data") +
   theme_bw()
 
 info <- animint2HTML(list(sepal = p1, petal = p2))
@@ -51,8 +51,8 @@ fillPattern <-
   paste0("fill: ",
          "(?<value>.*?)",
          ";")
-strokePattern <- paste0("stroke: ", 
-                        "(?<value>.*?)", 
+strokePattern <- paste0("stroke: ",
+                        "(?<value>.*?)",
                         ";")
 dasharrayPattern <-
   paste0("stroke-dasharray:",
@@ -62,13 +62,13 @@ dasharrayPattern <-
 test_color <- function(value, expected) {
   # convert R color to hexadecimal
   expected_col <- toRGB(expected)
-  
+
   if(grepl("rgb", value)) {
     expected_string <- paste0("rgb(", paste(col2rgb(expected_col), collapse = ", "), ")")
   } else {
     expected_string <- expected_col
   }
-  
+
   expect_equal(toupper(value), toupper(expected_string))
 }
 
@@ -76,12 +76,12 @@ test_that("panel backgrounds render correctly", {
   # testing that there are the correct number of panels
   expect_equal(length(background_sepal), 3)
   expect_equal(length(background_petal), 1)
-  
+
   # test background fills
   match_sepal <- str_match_perl(attr_back_sepal["style",], fillPattern)
   value_sepal <- match_sepal[, "value"]
   test_color(value_sepal[1], "lightblue")
-  
+
   match_petal <- str_match_perl(attr_back_petal["style",], fillPattern)
   value_petal <- match_petal[, "value"]
   test_color(value_petal[1], "white")
@@ -91,12 +91,12 @@ test_that("panel borders render correctly", {
   # testing that there are the correct number of panels
   expect_equal(length(border_sepal), 3)
   expect_equal(length(border_petal), 1)
-  
+
   # test border colors
   match_sepal <- str_match_perl(attr_border_sepal["style",], strokePattern)
   value_sepal <- match_sepal[, "value"]
   test_color(value_sepal[1], "black")
-  
+
   match_petal <- str_match_perl(attr_border_petal["style",], strokePattern)
   value_petal <- match_petal[, "value"]
   test_color(value_petal[1], "grey50")
@@ -108,4 +108,25 @@ test_that("grid lines are drawn correctly", {
   expect_equal(length(grid_major_petal), 9)
   expect_equal(length(grid_minor_sepal), 27)
   expect_equal(length(grid_minor_petal), 9)
+})
+
+test_that("renderer can handle no grid lines", {
+  # plot
+  data(tips, package = "reshape2")
+  tips$sex_smoker <- with(tips, interaction(sex, smoker))
+  info <- animint2HTML(list(
+    p1 = ggplot() + theme(legend.position = "none") +
+      geom_point(data = tips, position = "jitter", 
+                 aes(x = sex, y = smoker, colour = sex_smoker,
+                     clickSelects = sex_smoker)), 
+    p2 = ggplot() +
+      geom_point(data = tips,
+                 aes(x = total_bill, y = tip, colour = sex_smoker,
+                     showSelected = sex_smoker))
+    ))
+  # extract grids
+  grid_major_p1 <- getNodeSet(info$html, '//svg[@id="p1"]//line[@class="grid grid_major"]')
+  grid_minor_p1 <- getNodeSet(info$html, '//svg[@id="p1"]//line[@class="grid grid_minor"]')
+  expect_equal(length(grid_major_p1), 4)
+  expect_equal(length(grid_minor_p1), 0)
 })
