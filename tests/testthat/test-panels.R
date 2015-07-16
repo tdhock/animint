8ea9ee546cc28f5e934aefc364293ffea94fd2ca
kferris10 <kevin.ferris10@gmail.com>
2015-07-16 17:45:17 -0600

    Fix typo

--- tests/testthat/test-panels.R
+++ tests/testthat/test-panels.R
@@ -29,7 +29,7 @@ p4 <- p2 +
         panel.background = element_rect(), 
         panel.grid = element_line(), 
         panel.grid.major = element_line(), 
-        panel.grid.minor = element_blank(), 
+        panel.grid.minor = element_blank()
         )
 
 info <- animint2HTML(list(sepal = p1, petal = p2, blank = p3, gg538 = p4))
