520703fab8b2396271d603509f1cc09e39a45c81
Toby Dylan Hocking <tdhock5@gmail.com>
2015-07-22 11:50:02 -0400

    worldbank-good tests

---
+++ tests/testthat/test-WorldBank-good.R
@@ -0,0 +1,62 @@
+library(testthat)
+context("WorldBank-good")
+
+library(animint)
+data(WorldBank)
+
+not.na <- subset(WorldBank, !(is.na(life.expectancy) | is.na(fertility.rate)))
+subset(not.na, is.na(not.na$population))
+not.na[not.na$country=="Kuwait", "population"] <- 1700000
+
+## This example is good because it uses constancy
+## http://bost.ocks.org/mike/constancy/
+good <-
+  list(scatter=ggplot()+
+       geom_point(aes(life.expectancy, fertility.rate, clickSelects=country,
+                      showSelected=year, colour=region, size=population,
+                      tooltip=paste(country, "population", population),
+                      key=country), # key aesthetic for animated transitions!
+                  data=not.na)+
+       geom_text(aes(life.expectancy, fertility.rate, label=country,
+                     showSelected=country, showSelected2=year,
+                     key=country), #also use key here!
+                 data=not.na)+
+       scale_size_animint(breaks=10^(5:9))+
+       make_text(WorldBank, 55, 9, "year"),
+       ts=ggplot()+
+       make_tallrect(WorldBank, "year")+
+       geom_line(aes(year, life.expectancy, group=country, colour=region,
+                     clickSelects=country),
+                 data=WorldBank, size=4, alpha=3/5),
+       time=list(variable="year",ms=3000),
+       bar=ggplot()+
+       theme_animint(height=2400)+
+       geom_bar(aes(country, life.expectancy, fill=region,
+                    showSelected=year, clickSelects=country),
+                data=WorldBank, stat="identity", position="identity")+
+       coord_flip(),
+       duration=list(year=1000),
+       first=list(year=1975, country="United States"),
+       title="World Bank data (single selection)")
+
+info <- animint2HTML(good)
+
+test_that("bars render", {
+  node.set <-
+    getNodeSet(info$html, '//g[@class="geom6_bar_bar"]//rect')
+
+  expect_true(0 < length(node.set))
+
+  at.mat <- sapply(node.set, xmlAttrs)
+
+  num.vec <- as.numeric(at.mat[c("x", "width", "y", "height"), ])
+
+  expect_true(all(is.finite(num.vec)))
+})
+
+test_that("unspecified hjust means text-anchor: middle", {
+  style.value <-
+    getStyleValue(info$html, '//g[@class="geom2_text_scatter"]//text', 
+                  "text-anchor")
+  expect_match(style.value, "middle")
+})  
