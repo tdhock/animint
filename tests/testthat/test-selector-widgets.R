d0a4d244ea3408250924cbb94390f3d971d9d9a6
kferris10 <kevin.ferris10@gmail.com>
2015-08-16 08:57:00 -0600

    Start updating test for selector widgets

--- tests/testthat/test-selector-widgets.R
+++ tests/testthat/test-selector-widgets.R
@@ -1,15 +1,19 @@
 context("Selector Widgets")
 
-p1 <- ggplot() +
-  geom_point(aes(Sepal.Length, Sepal.Width, clickSelects = Species),
-             data = iris) +
-  ggtitle("Sepal Data")
-p2 <- ggplot() +
-  geom_point(aes(Petal.Length, Petal.Width, colour = Species, 
-                 showSelected = Species), data = iris) +
-  ggtitle("Petal Data")
+mtcars$cyl <- factor(mtcars$cyl)
+mtcars$vs <- factor(mtcars$vs)
+p1 <- ggplot() + 
+  geom_point(aes(wt, mpg, colour = cyl, size = vs), 
+             data = mtcars)
+p2  <- ggplot() + 
+  geom_point(aes(mpg, wt, colour = cyl, size = vs), data = mtcars)
 
-info <- animint2HTML(list(p1 = p1, p2 = p2))
+
+info <- animint2HTML(
+  list(p1 = p1, p2 = p2, 
+       selector.types = list(vs = "single", cyl = "multiple"), 
+       first = list(vs = 0, cyl = c(4,6)))
+)
 
 # different patterns to access
 fillPattern <-
@@ -28,13 +32,26 @@ test_fills <- function(fills, exp_color, exp_length) {
   expect_equal(matching_fills, exp_length)
 }
 
-test_that("Show/Hide Species widget renders", {
-  sp_widget <- getNodeSet(info$html, '//button[@id="Species"]')
-  expect_equal(length(sp_widget), 1)
+test_that("Widgets render", {
+  # testing that the "toggle selectors" button is present
+  toggle_widget <- getNodeSet(info$html, '//button[@id="show_hide_selector_widgets"]')
+  expect_equal(length(toggle_widget), 1)
+  
+  clickID("show_hide_selector_widgets")
+  # testing that the vs widget is present
+  vs_widget <- getNodeSet(info$html, '//select[@id="vs_input"]')
+  expect_equal(length(vs_widget), 1)
+  # testing that the cyl widget is present
+  cyl_widget <- getNodeSet(info$html, '//select[@id="cyl_input"]')
+  expect_equal(length(cyl_widget), 1)
 })
 
-test_that("Clicking displays button for each species", {
-  clickID("Species")
+test_that("Updating cyl widget shows/hides points", {
+  e <- remDr$findElement("id", "cyl_input")
+  e$clickElement()
+  e$clearElement()
+  browser()
+  e$sendKeysToElement(list("3000", key="enter"))
   buttons <- 
     getNodeSet(getHTML(), '//table[@class="selector_table"]//button')
   expect_equal(length(buttons), 3)
