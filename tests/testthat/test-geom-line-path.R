a284c300c737c8775dbb50176bc2591dc7456877
Toby Dylan Hocking <tdhock5@gmail.com>
2015-09-18 16:26:48 -0400

    do not depend on data.table and dplyr

--- tests/testthat/test-geom-line-path.R
+++ tests/testthat/test-geom-line-path.R
@@ -1,47 +1,74 @@
 library(testthat)
 context("geom line")
 library(animint)
-library(data.table)
-library(dplyr)
 
 data(intreg)
 signal.colors <- c(estimate="#0adb0a", latent="#0098ef")
 breakpoint.colors <- c("1breakpoint"="#ff7d7d", "0breakpoints"='#f6f4bf')
-segs <- data.table(intreg$segments)
-sigs <- data.table(intreg$signals) %>%
-  mutate(base.after=base+1)
-setkey(segs, signal, first.base, last.base)
-setkey(sigs, signal, base, base.after)
-ov <- foverlaps(sigs, segs)
-model.selection <- ov %>%
-  group_by(signal, segments) %>%
-  summarise(error=sum((logratio-mean)^2),
-            data=n()) %>%
-  mutate(log.data=log(data),
-         penalized.error=error + log.data * segments)
-sig.labels <- model.selection %>%
-  group_by(signal) %>%
-  filter(segments==1)
-sig.seg.names <- ov %>%
-  group_by(signal, segments) %>%
-  summarise(min.base=min(base),
-            max.base=max(base)) %>%
-  mutate(base=(min.base+max.base)/2)
-sig.names <- sig.seg.names %>%
-  group_by(signal, base) %>%
-  summarise(logratio=max(sigs$logratio))
-seg.names <- sig.seg.names %>%
-  group_by(signal, segments, base) %>%
-  summarise(logratio=min(sigs$logratio)-0.2)
-tallrects <- make_tallrect(model.selection, "segments")
-tallrects$geom_params$colour <- signal.colors[["estimate"]]
+models.by.signal <- with(intreg, split(segments, segments$signal))
+signals.by.signal <- with(intreg, split(signals, signals$signal))
+model.selection.list <- list()
+sig.labels.list <- list()
+sig.seg.names.list <- list()
+sig.names.list <- list()
+for(signal.name in names(segments.by.signal)){
+  signal <- signals.by.signal[[signal.name]]
+  signal.models <- models.by.signal[[signal.name]]
+  models.by.segments <- split(signal.models, signal.models$segments)
+  sig.seg.by.segments <- list()
+  for(segments.str in names(models.by.segments)){
+    segments <- as.numeric(segments.str)
+    model <- models.by.segments[[segments.str]]
+    model$rss <- NA
+    model$data <- NA
+    for(segment.i in 1:nrow(model)){
+      one.segment <- model[segment.i, ]
+      seg.data <- 
+        subset(signal,
+               one.segment$first.base < base &
+                 base < one.segment$last.base)
+      model$data[segment.i] <- nrow(seg.data)
+      residual.vec <- seg.data$logratio - one.segment$mean
+      model$rss[segment.i] <- sum(residual.vec * residual.vec)
+    }
+    stopifnot(sum(model$data) == nrow(signal))
+    model.stats <- 
+      data.frame(signal=signal.name,
+                 segments,
+                 error=sum(model$rss),
+                 data=sum(model$data))
+    model.selection.list[[paste(signal.name, segments.str)]] <- model.stats
+    if(segments.str == "1"){
+      sig.labels.list[[signal.name]] <- model.stats
+    }
+    sig.seg <-
+      data.frame(signal=signal.name,
+                 segments,
+                 min.logratio=min(intreg$signals$logratio)-0.2,
+                 max.logratio=max(intreg$signals$logratio),
+                 base=(min(signal$base)+max(signal$base))/2)
+    sig.seg.by.segments[[segments.str]] <- sig.seg
+  }
+  sig.seg.tall <- do.call(rbind, sig.seg.by.segments)
+  sig.seg.names.list[[signal.name]] <- sig.seg.tall
+  sig.names.list[[signal.name]] <- sig.seg.tall[1,]
+}
+model.selection.sorted <- do.call(rbind, model.selection.list)
+set.seed(1)
+model.selection <-
+  model.selection.sorted[sample(1:nrow(model.selection.sorted)),]
+sig.seg.names <- do.call(rbind, sig.seg.names.list)
+sig.names <- do.call(rbind, sig.names.list)
+sig.labels <- do.call(rbind, sig.labels.list)
 
 ## Plot segments rather than penalty.
 mmir.selection <- 
   list(error=ggplot()+
        ggtitle("Select profile and number of segments")+
-       tallrects+
+       make_tallrect(model.selection, "segments",
+                     colour=signal.colors[["estimate"]])+
        theme_bw()+
+       theme_animint(width=600)+
        theme(panel.margin=grid::unit(0, "lines"))+
        facet_grid(. ~ geom)+
        geom_text(aes(0, error, label=signal, clickSelects=signal),
@@ -61,6 +88,7 @@ mmir.selection <-
                  alpha=0.6, size=8),
 
        signal=ggplot()+
+         theme_bw()+
        theme_animint(width=800)+       
        scale_x_continuous("position on chromosome (mega base pairs)",
                           breaks=c(100,200))+
@@ -82,15 +110,15 @@ mmir.selection <-
                   colour=signal.colors[["estimate"]],
                   linetype="dashed",
                   data=intreg$breaks)+
-       geom_text(aes(base/1e6, logratio, label=paste("signal", signal),
+       geom_text(aes(base/1e6, max.logratio, label=paste("signal", signal),
                      showSelected=signal),
                  data=sig.names)+
-       geom_text(aes(base/1e6, logratio,
+       geom_text(aes(base/1e6, min.logratio,
                      label=sprintf("%d segment%s", segments,
                        ifelse(segments==1, "", "s")),
                      showSelected=signal,
                      showSelected2=segments),
-                 data=seg.names, color=signal.colors[["estimate"]]),
+                 data=sig.seg.names, color=signal.colors[["estimate"]]),
 
        first=list(signal="4.2", segments=4))
 
@@ -100,18 +128,23 @@ all.increasing <- function(num.vec){
   stopifnot(is.numeric(num.vec))
   all(0 < diff(num.vec))
 }
-
 expected.list <-
-  list(geom3_line_error=function(x)all.increasing(x),
-       geom4_path_error=function(x)!all.increasing(x))
-
+  list(geom3_line_error=TRUE,
+       geom4_path_error=FALSE)
+result.list <- list()
 for(g.class in names(expected.list)){
-  should.return.true <- expected.list[[g.class]]
+  expected <- expected.list[[g.class]]
   tsv.path <- Sys.glob(file.path("intreg-selection", paste0(g.class, "*")))
   g.data <- read.table(tsv.path, header=TRUE)
   tsv.by.signal <- split(g.data, g.data$clickSelects)
   for(signal.name in names(tsv.by.signal)){
     one.signal <- tsv.by.signal[[signal.name]]
-    expect_true(should.return.true(one.signal$x))
+    computed <- all.increasing(one.signal$x)
+    result.list[[paste(g.class, signal.name)]] <-
+      data.frame(g.class, signal.name, computed, expected)
   }
 }
+result <- do.call(rbind, result.list)
+with(result, {
+  expect_identical(computed, expected)
+})
