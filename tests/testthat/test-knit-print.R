af833969932611bfe4c84630d293face2c766cf1
Toby Dylan Hocking <tdhock5@gmail.com>
2015-09-23 17:16:42 -0400

    two interactive plots test fails

--- tests/testthat/test-knit-print.R
+++ tests/testthat/test-knit-print.R
@@ -3,36 +3,58 @@ context("knitting multiple animint plots in a single Rmd")
 knitr::knit_meta() #clear knitr 'metadata'
 test_file <- system.file("examples", "test_knit_print.Rmd", 
                          package = "animint")
+
 setwd("animint-htmltest")
 knitr::knit2html(input = test_file, output = "index.html")
 setwd("..")
 remDr$refresh()
 html <- getHTML()
 
-test_that("knit_print.animint works as intended", {
+test_that("knit_print.animint renders three plots", {
   nodes <- getNodeSet(html, "//text[@id='xtitle']")
-  xlabel1 <- xmlValue(nodes[[1]])
-  expect_match(xlabel1, "Worthless label 1")
-  xlabel2 <- xmlValue(nodes[[2]])
-  expect_match(xlabel2, "Worthless label 2")
+  value.vec <- sapply(nodes, xmlValue)
+  expected.vec <-
+    c("first plot with color legend",
+      "second plot with color legend",
+      "non-interactive plot")
+  expect_identical(value.vec, expected.vec)
 })
 
-test_that("legend interactivity works in an Rmd document", {
-  ## function to extract all circles from an HTML page
-  get_circles <- function(id) {
-    getNodeSet(getHTML(), "//circle[@class='geom']")
-  }
-  
-  # 10 circles on the plot
+## function to extract all circles from an HTML page
+get_circles <- function(id) {
+  getNodeSet(getHTML(), "//circle[@class='geom']")
+}
+
+get_elements <- function(id){
+  div <- remDr$findElement("id", id)
+  list(a178=div$findChildElement("id", "a178"),
+       b934=div$findChildElement("id", "b934"))
+}
+
+plot1top <- get_elements("plot1top")
+plot1bottom <- get_elements("plot1bottom")
+
+test_that("clicking top legend adds/remove points", {
+  expect_equal(length(get_circles()), 20)
+  plot1top$a178$clickElement()
+  expect_equal(length(get_circles()), 15)
+  plot1top$b934$clickElement()
   expect_equal(length(get_circles()), 10)
-  
-  # only 5 circles on the plot after clicking
-  clickID("a178")
-  expect_equal(length(get_circles()), 5)
-  
-  # clicking again goes back to 10 plot circles
-  clickID("a178")
+  plot1top$b934$clickElement()
+  expect_equal(length(get_circles()), 15)
+  plot1top$a178$clickElement()
+  expect_equal(length(get_circles()), 20)
+})
+
+test_that("clicking bottom legend adds/remove points", {
+  expect_equal(length(get_circles()), 20)
+  plot1bottom$a178$clickElement()
+  expect_equal(length(get_circles()), 15)
+  plot1bottom$b934$clickElement()
   expect_equal(length(get_circles()), 10)
-  
+  plot1bottom$b934$clickElement()
+  expect_equal(length(get_circles()), 15)
+  plot1bottom$a178$clickElement()
+  expect_equal(length(get_circles()), 20)
 })
 
