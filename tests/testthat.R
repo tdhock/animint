8c5bfabcb3cde7aa23cefbf88b49066814acef08
Toby Dylan Hocking <tdhock5@gmail.com>
2015-09-28 08:03:08 -0400

    note firefox version used for testing

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,6 +2,11 @@ library("testthat")
 library("animint")
 library("RSelenium") # RSelenium works with firefox <= 37.0.2
 
+## TDH usually tests using 
+
+## thocking@silene:~/R/animint-mine(fix-common-chunk)$ firefox --version
+## Mozilla Firefox 11.0
+
 tests_init()
 tests_run()
 tests_exit()
