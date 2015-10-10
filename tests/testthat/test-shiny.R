173e6d45f3f436467b6e2aebd6a109191e110b49
Toby Dylan Hocking <tdhock5@gmail.com>
2015-10-10 15:44:50 -0400

    if(on travis) no longer needed

--- tests/testthat/test-shiny.R
+++ tests/testthat/test-shiny.R
@@ -1,44 +1,45 @@
 acontext("shiny")
 
-if (Sys.getenv("TRAVIS") == "true" | Sys.getenv("WERCKER") == "true") {
-  message("shiny tests don't work on travis/wercker (but should someday)")
-} else {
-  # shiny tests require navigating to different ports, so remember where we are
-  # and return when tests are done
-  old_address <- remDr$getCurrentUrl()[[1]]
-  remDr$setImplicitWaitTimeout(milliseconds = 30000)
-  
-  shiny_dir <- system.file("examples/shiny", package = "animint")
-  shiny_cmd <- "shiny::runApp(appDir=\"%s\", port=%d, launch.browser=FALSE)"
-  animint:::run_servr(port = 3147, directory = shiny_dir, code = shiny_cmd)
-  address <- sprintf("http://localhost:3147")
-  
-  test_that("animint plot renders in a shiny app", {
-    Sys.sleep(10) # give shiny a second to do it's thing
-    remDr$navigate(address)
-    Sys.sleep(10)
-    # just check that svg is displayed
-    html <- getHTML()
-    circles <- getNodeSet(html, "//div[@id='animint']//circle")
-    expect_true(length(circles) >= 1)
-  })
-  
-  rmd_dir <- system.file("examples/rmarkdown", package = "animint")
-  rmd_cmd <- "rmarkdown::run(dir = \"%s\", shiny_args = list(port=%d, launch.browser=FALSE))"
-  animint:::run_servr(port = 3120, directory = rmd_dir, code = rmd_cmd)
-  address <- sprintf("http://localhost:3120")
-  
-  test_that("animint plot renders in an interactive document", {
-    Sys.sleep(10) # give shiny a second to do it's thing
-    remDr$navigate(address)
-    Sys.sleep(10)
-    e <- remDr$findElement("class name", "shiny-frame")
-    remDr$switchToFrame(e)
-    html <- getHTML()
-    circles <- getNodeSet(html, "//svg//circle")
-    expect_true(length(circles) >= 1)
-  })
-  
-  # go back to non-shiny tests
-  remDr$navigate(old_address)
-}
+## We do not need if(on wercker or travis){skip shiny test} as of 10
+## Oct 2015, since we only run tests that match the TEST_SUITE env
+## var, and test-shiny.R never matches.
+
+## shiny tests require navigating to different ports, so remember
+## where we are and return when tests are done
+old_address <- remDr$getCurrentUrl()[[1]]
+remDr$setImplicitWaitTimeout(milliseconds = 30000)
+
+shiny_dir <- system.file("examples/shiny", package = "animint")
+shiny_cmd <- "shiny::runApp(appDir=\"%s\", port=%d, launch.browser=FALSE)"
+animint:::run_servr(port = 3147, directory = shiny_dir, code = shiny_cmd)
+address <- sprintf("http://localhost:3147")
+
+test_that("animint plot renders in a shiny app", {
+  Sys.sleep(10) # give shiny a second to do it's thing
+  remDr$navigate(address)
+  Sys.sleep(10)
+  ## just check that svg is displayed
+  html <- getHTML()
+  circles <- getNodeSet(html, "//div[@id='animint']//circle")
+  expect_true(length(circles) >= 1)
+})
+
+rmd_dir <- system.file("examples/rmarkdown", package = "animint")
+rmd_cmd <- "rmarkdown::run(dir = \"%s\", shiny_args = list(port=%d, launch.browser=FALSE))"
+animint:::run_servr(port = 3120, directory = rmd_dir, code = rmd_cmd)
+address <- sprintf("http://localhost:3120")
+
+test_that("animint plot renders in an interactive document", {
+  Sys.sleep(10) # give shiny a second to do it's thing
+  remDr$navigate(address)
+  Sys.sleep(10)
+  e <- remDr$findElement("class name", "shiny-frame")
+  remDr$switchToFrame(e)
+  html <- getHTML()
+  circles <- getNodeSet(html, "//svg//circle")
+  expect_true(length(circles) >= 1)
+})
+
+## go back to non-shiny tests
+remDr$navigate(old_address)
+
