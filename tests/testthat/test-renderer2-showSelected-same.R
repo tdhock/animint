c869198a8dd4ecb9de5f3b80e2db08f939c71e53
Toby Dylan Hocking <tdhock5@gmail.com>
2015-10-18 13:04:47 -0400

    redundant data not saved to tsv fails

--- tests/testthat/test-renderer2-showSelected-same.R
+++ tests/testthat/test-renderer2-showSelected-same.R
@@ -8,10 +8,21 @@ viz <- list(
                     data=iris)
   )
 info <- animint2HTML(viz)
+ss.list <- with(info$geoms$geom1_point_points, c(chunk_order, subset_order))
+ss.vec <- unlist(ss.list)
 
 test_that("redundant showSelected are optimized out", {
-  var.list <- with(info$geoms$geom1_point_points, c(chunk_order, subset_order))
-  expect_equal(length(var.list), 1)
+  expect_equal(length(ss.vec), 1)
+})
+
+test_that("redundant aes not saved to tsv", {
+  geom.tsv <- Sys.glob(
+    file.path("animint-htmltest", "geom1_point_points_*.tsv"))
+  expect_equal(length(geom.tsv), 1)
+  expected.names <- sort(c("x", "y", ss.vec))
+  tsv.data <- read.table(geom.tsv, header=TRUE)
+  computed.names <- sort(names(tsv.data))
+  expect_identical(computed.names, expected.names)
 })
 
 test_that("selector widgets table initially visiable", {
