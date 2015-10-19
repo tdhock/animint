12d948059e903e36bafdcdd42f80b2748f88755b
Toby Dylan Hocking <tdhock5@gmail.com>
2015-10-19 11:33:56 -0400

    abline tsv test fails

--- tests/testthat/test-renderer1-geom-abline.R
+++ tests/testthat/test-renderer1-geom-abline.R
@@ -4,6 +4,16 @@ p <- qplot(wt, mpg, data = mtcars) +
   geom_abline(intercept = c(20, 5), slope = c(1,4)) + 
   facet_wrap(~cyl)
 info <- animint2HTML(list(p = p))
+
+tsv.file <- file.path("animint-htmltest", "geom2_abline_p_chunk1.tsv")
+tsv.data <- read.table(tsv.file, header=TRUE)
+
+test_that("columns of abline tsv", {
+  expected.names <- sort(c("PANEL", "x", "xend", "y", "yend"))
+  computed.names <- sort(names(tsv.data))
+  expect_identical(computed.names, expected.names)
+})
+
 ablines <- getNodeSet(info$html, '//svg//g[@class="geom2_abline_p"]//line')
 attr_ablines <- sapply(ablines, xmlAttrs)
 start_ends <- attr_ablines[c("x1", "x2", "y1", "y2"), ]
