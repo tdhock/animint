6ba186a22e4aa71d9871cb56b96a268da005a943
Toby Dylan Hocking <tdhock5@gmail.com>
2015-11-03 11:17:19 -0500

    compiler should stop with an error on multiple showSelected

---
+++ tests/testthat/test-compiler-errors.R
@@ -0,0 +1,14 @@
+acontext("compiler errors")
+
+test_that("aes(showSelected=var1, showSelected=var2) is an error", {
+  viz <- list(
+    petals=ggplot()+
+      geom_point(aes(Petal.Width, Petal.Length,
+                     showSelected=Species,
+                     showSelected=Sepal.Width),
+                 data=iris)
+    )
+  expect_error({
+    animint2dir(viz)
+  }, "aes names must be unique")
+})
