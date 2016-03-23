467ab7c02f8083af05e0fa1f00fdf86fc2d18863
Toby Dylan Hocking <tdhock5@gmail.com>
2016-03-23 07:36:14 -0400

    informative error test fails

--- tests/testthat/test-compiler-errors.R
+++ tests/testthat/test-compiler-errors.R
@@ -12,3 +12,25 @@ test_that("aes(showSelected=var1, showSelected=var2) is an error", {
     animint2dir(viz)
   }, "aes names must be unique, problems: showSelected")
 })
+
+test_that("informative error for time option with no showSelected", {
+  viz <- list(
+    petals=ggplot()+
+      geom_point(aes(Petal.Width, Petal.Length),
+                 data=iris),
+    time=list(variable="Species", ms=3000)
+  )
+  expect_error({
+    animint2dir(viz)
+  }, "no interactive aes for time variable Species")
+})
+
+test_that("no error for time option with clickSelects", {
+  viz <- list(
+    petals=ggplot()+
+      geom_point(aes(Petal.Width, Petal.Length, clickSelects=Species),
+                 data=iris),
+    time=list(variable="Species", ms=3000)
+  )
+  info <- animint2dir(viz)
+})
